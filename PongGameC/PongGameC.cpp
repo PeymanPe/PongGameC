248db2812c4fac74a2ca7f7935d77828a344f607
Peyman Peirovifar <peirovifar@gmail.com>
2023-02-11 01:06:42 +0330

    Added a ball but I need to figure out how to update the current location

--- PongGameC/PongGameC.cpp
+++ PongGameC/PongGameC.cpp
@@ -19,11 +19,38 @@ using namespace std;
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // nanoseconds, system_clock, seconds
 
-void setupgame(int inputkey=0) {
+void centerCircle(Point* arr, int* direction) {
+	Point a1(1, 1);
+	Point a2(-1, 1);
+	Point a3(1, -1);
+	Point a4(-1, -1);
+	if (*direction == 1) {
+		*arr = a1;
+		
+	}
+	else if (*direction == 2) {
+		*arr = a2;
+		
+	}
+	else if (*direction == 3) {
+		*arr = a3;
+		
+	}
+	else if (*direction == 4) {
+		*arr = a4;
+		
+	}
+
+
+}
+
+void setupgame(int inputkey=0, int counter=0) {
 
 
 
+	srand(time(0));
 
+	int direction = 1;
 
 
 
@@ -40,6 +67,33 @@ void setupgame(int inputkey=0) {
 
 
 	rectangle(image, r2, Scalar(0, 255, 0), 10, 8, 0);
+
+
+	Point center(100, 100);
+	
+	Point adder(1, 1);
+	
+
+	if (counter == 0) {
+		Point center( 100, 100);//Declaring the center point
+	}
+	else  {
+		int cent[2] = { 100,100 };
+		centerCircle(&adder, &direction);
+		center = center + adder;
+		
+		
+
+	}
+	
+	int radius = 5; //Declaring the radius
+	Scalar line_Color(0, 0, 0);//Color of the circle
+	int thickness = 5;//thickens of the line
+	
+	circle(image, center, radius, line_Color, thickness);
+
+
+
 	imshow("Display Window", image);
 	
 	sleep_until(system_clock::now() + seconds(1));
@@ -50,17 +104,19 @@ void setupgame(int inputkey=0) {
 }
 
 
+
 int main(int argc, char* argv[])
 {
 	int ch;
 	int incc=10;
 	int i = 0;
+	
 
 
 	while (i < 10) {
 		
 		incc *= (-1);
-		setupgame(incc);
+		setupgame(incc,i);
 		waitKey(100);
 	
 		i++;
